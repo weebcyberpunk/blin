69ef9a2a2f46aeef741c8208aa0c8ab9faed7815
GG <weebcyberpunk@gmail.com>
2022-07-07 12:45:31 -0300

    Swapped tabs for spaces

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Blin
 
-Blin stands for "Breakline in". Started as an exercise, I actually founded it very useful. It receives input from stdin or file and copies it to stdout or file, breaking lines in the last blank before an character limit.
+Blin stands for "Breakline in". Started as an exercise, I actually founded it
+very useful. It receives input from stdin or file and copies it to stdout or
+file, breaking lines in the last blank before an character limit.
 
 ## Installation
 
-Arch Linux and Arch-based Linux distributions users can install it from AUR with the package `blin-git`.  
-Others can install via `# make install` and uninstall via `# make uninstall`
+Arch Linux and Arch-based Linux distributions users can install it from AUR with
+the package `blin-git`.  Others can install via `# make install` and uninstall
+via `# make uninstall`
 
 ## Usage
 
-The basic use is: `$ blin <num>`. This will receive input from _stdin_ and print to _stdout_, breaking the lines in the last blank character before the character limit.
+The basic use is: `$ blin <num>`. This will receive input from _stdin_ and print
+to _stdout_, breaking the lines in the last blank character before the character
+limit.
 
-You can specify files too: `$ blin <num> <input file> <output file>` will receive input from `input file` and print to `output file`. If one of the files specified is `-`, it defaults to _stdin_ or _stdout_.
+You can specify files too: `$ blin <num> <input file> <output file>` will
+receive input from `input file` and print to `output file`. If one of the files
+specified is `-`, it defaults to _stdin_ or _stdout_.
--- main.c
+++ main.c
@@ -32,121 +32,121 @@
 
 // prints num chars from *bp
 int print_buf(char *bp, unsigned long num, FILE *output) {
-	for (unsigned long count = 0; count < num; count++)
-		fprintf(output, "%c", bp[count]);
+    for (unsigned long count = 0; count < num; count++)
+        fprintf(output, "%c", bp[count]);
 
-	return(0);
+    return(0);
 }
 
 int main(int argc, char *argv[]) {
 
-	char c;
-	unsigned long lw;
-	unsigned long length = 0;
-	unsigned long wlength = 0;
-	unsigned long last_blank = 0;
-	char *lbuf;
-	char *wbuf;
-	short newline_from_before = 0;
-	FILE *in;
-	FILE *out;
+    char c;
+    unsigned long lw;
+    unsigned long length = 0;
+    unsigned long wlength = 0;
+    unsigned long last_blank = 0;
+    char *lbuf;
+    char *wbuf;
+    short newline_from_before = 0;
+    FILE *in;
+    FILE *out;
 
-	// get fw
-	if (argc > 1) {
-		lw = atoi(argv[1]);
+    // get fw
+    if (argc > 1) {
+        lw = atoi(argv[1]);
 
-	} else {
-		fprintf(stderr, "Please specify line width.\n");
-		return(1);
+    } else {
+        fprintf(stderr, "Please specify line width.\n");
+        return(1);
 
-	}
+    }
 
-	// get files
-	in = stdin;
-	out = stdout;
+    // get files
+    in = stdin;
+    out = stdout;
 
-	if (argc >= 4) {
-		if (strcmp(argv[2], "-") != 0) in = fopen(argv[2], "r");
-		if (strcmp(argv[3], "-") != 0) out = fopen(argv[3], "w");
+    if (argc >= 4) {
+        if (strcmp(argv[2], "-") != 0) in = fopen(argv[2], "r");
+        if (strcmp(argv[3], "-") != 0) out = fopen(argv[3], "w");
 
-	} else if (argc == 3) {
-		if (strcmp(argv[2], "-") != 0) in = fopen(argv[2], "r");
+    } else if (argc == 3) {
+        if (strcmp(argv[2], "-") != 0) in = fopen(argv[2], "r");
 
-	}
+    }
 
-	// alloc memory
-	lbuf = malloc(lw);
-	wbuf = malloc(lw);
+    // alloc memory
+    lbuf = malloc(lw);
+    wbuf = malloc(lw);
 
-	// main loop
-	for (;;) {
-		c = getc(in);
+    // main loop
+    for (;;) {
+        c = getc(in);
 
-		if (c == '\n' || c == EOF) {
-			if (c == '\n')
-				if (newline_from_before) continue;
+        if (c == '\n' || c == EOF) {
+            if (c == '\n')
+                if (newline_from_before) continue;
 
-			// prints buffer and resets vars
-			print_buf(lbuf, length, out);
-			fprintf(out, "\n");
-			length = 0;
-			wlength = 0;
-			last_blank = 0;
+            // prints buffer and resets vars
+            print_buf(lbuf, length, out);
+            fprintf(out, "\n");
+            length = 0;
+            wlength = 0;
+            last_blank = 0;
 
-			if (c == EOF) break;
+            if (c == EOF) break;
 
-		} else if (c == ' ') {
-			// sets last_blank and resets wlength,
-			// increases length and adds new char
-			last_blank = last_blank + wlength + 1;
-			wlength = 0;
+        } else if (c == ' ') {
+            // sets last_blank and resets wlength,
+            // increases length and adds new char
+            last_blank = last_blank + wlength + 1;
+            wlength = 0;
 
-			lbuf[length] = c;
-			length++;
+            lbuf[length] = c;
+            length++;
 
-		} else {
-			// moves new char and increases vars
-			wbuf[wlength] = c;
-			wlength++;
+        } else {
+            // moves new char and increases vars
+            wbuf[wlength] = c;
+            wlength++;
 
-			lbuf[length] = c;
-			length++;
-		}
+            lbuf[length] = c;
+            length++;
+        }
 
-		if ((length == lw) && (c != '\n')) {
-			// if word > lw
-			if (wlength == lw) {
-				print_buf(lbuf, length, out);
-				fprintf(out, "\n");
-				newline_from_before = 1;
+        if ((length == lw) && (c != '\n')) {
+            // if word > lw
+            if (wlength == lw) {
+                print_buf(lbuf, length, out);
+                fprintf(out, "\n");
+                newline_from_before = 1;
 
-				length = 0;
-				wlength = 0;
-				last_blank = 0;
+                length = 0;
+                wlength = 0;
+                last_blank = 0;
 
-			// prints buffer
-			} else {
-				print_buf(lbuf, last_blank, out);
-				fprintf(out, "\n");
-				newline_from_before = 1;
+            // prints buffer
+            } else {
+                print_buf(lbuf, last_blank, out);
+                fprintf(out, "\n");
+                newline_from_before = 1;
 
-				// resets vars
-				length = 0;
-				last_blank = 0;
+                // resets vars
+                length = 0;
+                last_blank = 0;
 
-				// adds word to lbuf
-				for (int count = 0; count < wlength; count++) {
-					lbuf[count] = wbuf[count];
-					length++;
+                // adds word to lbuf
+                for (int count = 0; count < wlength; count++) {
+                    lbuf[count] = wbuf[count];
+                    length++;
 
-				}
-			}
+                }
+            }
 
-			continue;
-		}
+            continue;
+        }
 
-		newline_from_before = 0;
-	}
+        newline_from_before = 0;
+    }
 
-	return(0);
+    return(0);
 }
